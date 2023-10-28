c603cfd30b0f3026195c4ade54e7bc964b9b1314
uttamdhokiya2003 <uttamdhokiya84@gmail.com>
2023-10-28 11:24:05 +0530

    Initial commit

---
+++ Areas/SEC_User/Controllers/SEC_UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddEditMetronic8.Areas.SEC_User.Controllers
+{
+	[Area("SEC_User")]
+	[Route("[Controller]/[action]")]
+	
+	public class SEC_UserController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ DAL/DAL_Helper.cs
@@ -0,0 +1,8 @@
+﻿namespace AddEditMetronic8.DAL
+{
+    public class DAL_Helper
+    {
+        public static string ConnectionStr = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("myConnectionString");
+
+    }
+}
